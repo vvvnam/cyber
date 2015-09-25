62a6a11b4310aedcb9aeb47c367b89c0e67f2399
RUBY <vvvnam@hanmail.com>
2015-09-25 16:33:45 +0900

    test3

---
+++ src/test20150925/test/test3.java
@@ -0,0 +1,11 @@
+package test20150925.test;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("ddddddddddddddddddoooooooooooooooooo");
+	}
+
+}
